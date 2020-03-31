4dd65804c0eda809c85fcf73b5a031c936a6f614
Yzngo <yzngo@live.com>
2020-03-31 20:16:08 +0800

    update

--- tablegen2/logic/parser/TableExcelExportJson.cs
+++ tablegen2/logic/parser/TableExcelExportJson.cs
@@ -10,23 +10,102 @@ namespace tablegen2.logic
     {
         public static void exportExcelFile(TableExcelData data, string filePath)
         {
-            List<Dictionary<string, object>> lst = ExportData(data, string.Empty);
+            List<Dictionary<string, object>> lst = ExportData(data);
 
             var indent = AppData.Config.OutputLuaWithIndent ? Formatting.Indented : Formatting.None;
             string output = JsonConvert.SerializeObject(lst, indent);
             File.WriteAllBytes(filePath, Encoding.UTF8.GetBytes(output));
         }
 
-        public static List<Dictionary<string, object>> ExportData(TableExcelData data, string key)
+        public static List<Dictionary<string, object>> ExportData(TableExcelData data)
         {
             List<Dictionary<string, object>> lst = new List<Dictionary<string, object>>();
 
             foreach (var row in data.Rows)
             {
                 var r = new Dictionary<string, object>();
+
+                for (int i = 0; i < data.Headers.Count; i++)
+                {
+                    var hdr = data.Headers[i];
+                    var val = row.StrList[i];
+
+                    if (string.IsNullOrEmpty(val) && !(hdr.FieldType == "group" || hdr.FieldType == "string" || hdr.FieldType == "table"))
+                    {
+                        continue;
+                    }
+                    object obj = null;
+                    switch (hdr.FieldType)
+                    {
+                        case "string":
+                        case "string(nil)":
+                        case "color":
+                            obj = val;
+                            break;
+                        case "int":
+                            {
+                                int.TryParse(val, out int n);
+                                obj = n;
+                            }
+                            break;
+                        case "double":
+                            {
+                                double.TryParse(val, out double n);
+                                obj = n;
+                            }
+                            break;
+                        case "bool":
+                            {
+                                bool.TryParse(val, out bool n);
+                                obj = n;
+                            }
+                            break;
+                        case "group":
+                            {
+                                var str = val.Split(',');
+                                bool succeed = false;
+                                {
+                                    var numlist = new List<double>();
+                                    foreach (var s in str)
+                                    {
+                                        succeed = double.TryParse(s, out double n);
+                                        numlist.Add(n);
+                                    }
+                                    obj = numlist;
+                                }
+                                
+                                if (succeed == false)
+                                {
+                                    var strlist = new List<string>();
+                                    foreach (var s in str)
+                                    {
+                                        strlist.Add(s.Replace("\"", string.Empty));
+                                    }
+                                    obj = strlist;
+                                }
+                            }
+                            break;
+                        case "table":
+                            {
+                                obj = ExportDataSub(data.ChildData[hdr.FieldName], row.StrList[1]);
+                                break;
+                            }
+                    }
+                    r[hdr.FieldName] = obj;
+                }
+                lst.Add(r);
+            }
+            return lst;
+        }
+
+        public static Dictionary<string, object> ExportDataSub(TableExcelData data, string key)
+        {
+            var r = new Dictionary<string, object>();
+            foreach (var row in data.Rows)
+            {
                 if (key != string.Empty && key != row.StrList[1])
                 {
-                    break;
+                    continue;
                 }
                 for (int i = 0; i < data.Headers.Count; i++)
                 {
@@ -69,27 +148,38 @@ namespace tablegen2.logic
                         case "group":
                             {
                                 var str = val.Split(',');
-                                var num = new List<double>();
-                                foreach (var s in str)
+                                bool succeed = false;
                                 {
-                                    double.TryParse(s, out double n);
-                                    num.Add(n);
+                                    var numlist = new List<double>();
+                                    foreach (var s in str)
+                                    {
+                                        succeed = double.TryParse(s, out double n);
+                                        numlist.Add(n);
+                                    }
+                                    obj = numlist;
                                 }
-                                obj = num;
+
+                                if (succeed == false)
+                                {
+                                    var strlist = new List<string>();
+                                    foreach (var s in str)
+                                    {
+                                        strlist.Add(s.Replace("\"", string.Empty));
+                                    }
+                                    obj = strlist;
+                                }
                             }
                             break;
                         case "table":
                             {
-                                obj = ExportData(data.ChildData[hdr.FieldName], row.StrList[1]);
+                                obj = ExportDataSub(data.ChildData[hdr.FieldName], row.StrList[1]);
                                 break;
                             }
                     }
                     r[hdr.FieldName] = obj;
                 }
-                lst.Add(r);
-                //return r;
-            }//).ToList();
-            return lst;
+            }
+            return r;
         }
     }
 }
